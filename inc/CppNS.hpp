a281faa31ddf772e0c1dc88d44c8878a8bca629f
Pierre-Yves Taunay <py.taunay@psu.edu>
2013-11-15 11:44:09 -0500

    Work on GMRES to include generic templates

--- inc/CppNS.hpp
+++ inc/CppNS.hpp
@@ -2,5 +2,10 @@
 #define _CPPNS_HPP_
 
 #include <linear_algebra/containers/Vector.hpp>
+#include <linear_algebra/containers/SparseMatrix.hpp>
+
+#include <numerical_solvers/linear/GMRES.hpp>
+#include <numerical_solvers/linear/LinearSolver.hpp>
+
 
 #endif 
---
+++ inc/linear_algebra/containers/SparseMatrix.hpp
@@ -0,0 +1,19 @@
+#ifndef _SPARSEMATRIX_HPP_
+#define _SPARSEMATRIX_HPP_
+
+#include <iostream>
+#include <cuda.h>
+
+namespace LinearAlgebra {
+	namespace Containers {
+		template<typename T>
+		class SparseMatrix {
+			protected:
+
+			public:
+
+		};
+	}
+}	
+
+#endif
